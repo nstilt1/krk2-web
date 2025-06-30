0cc57f8abf6b226702142e978c580fd3a2446640
Noah Stiltner <nstilt1@lsu.edu>
2025-06-30 12:09:40 -0500

    update

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
       <head>
       <script 
         src="https://cdn.jsdelivr.net/combine/npm/tone@14.7.58,npm/@magenta/music@1.23.1/es6/core.js,npm/focus-visible@5,npm/html-midi-player@1.5.0"
-        async
+        async 
       ></script>
       </head>
       <body
